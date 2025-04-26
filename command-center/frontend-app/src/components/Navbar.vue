1c23c6369ed331185a5253ff2c9284b464fb3ca8
Dinas Informasi dan Pengolahan Data TNI Angkatan Darat <mariosec1337@gmail.com>
2025-04-26 18:49:19 +0700

    Create Navbar.vue

---
+++ command-center/frontend-app/src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a href="/" class="logo">Sentinel-Garuda</a>
+    </div>
+    <ul class="navbar-links">
+      <li><a href="/">Beranda</a></li>
+      <li><a href="/missions">Misi</a></li>
+      <li><a href="/profile">Profil</a></li>
+      <li><a href="/about">Tentang</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #343a40;
+  padding: 15px 30px;
+  color: white;
+}
+
+.navbar-brand .logo {
+  font-size: 1.5em;
+  color: white;
+  text-decoration: none;
+}
+
+.navbar-links {
+  list-style: none;
+  display: flex;
+}
+
+.navbar-links li {
+  margin-left: 20px;
+}
+
+.navbar-links a {
+  color: white;
+  text-decoration: none;
+  font-size: 1.1em;
+}
+
+.navbar-links a:hover {
+  text-decoration: underline;
+}
+</style>
